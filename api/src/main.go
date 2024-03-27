853c6b3a502998cdc7e95f1379cc21872c9706f7
Tawanda <ejmunongo@gmail.com>
2024-03-28 00:18:37 +0200

    Add Echo

--- api/src/main.go
+++ api/src/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"github.com/labstack/echo/v4"
+
+	"github.com/tmunongo/linkkeep/api/auth"
 )
 
 func main() {
-	fmt.Println("Hello!")
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello, World!")
+	})
+
+	authRoutes := e.Group("/auth")
+	auth.RegisterHandlers(authRoutes)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
+
